452522ac8749b05fd0e4ea63d1d81ff55a86cb52
赵卫国 <zhaoweiguo@lczq.com>
2022-12-22 14:25:02 +0800

    1. redis key生成更改为crc32，减少长度 2. redis key 前缀增加 selectId 值 3. 增减删除 redis key 的接口

--- src/main/java/com/lczq/dbquery/controller/DBMgtController.java
+++ src/main/java/com/lczq/dbquery/controller/DBMgtController.java
@@ -5,18 +5,21 @@ import com.lczq.dbquery.entities.QueryParams;
 import com.lczq.dbquery.entities.SignEntity;
 import com.lczq.dbquery.mapper.QueryMapper;
 import com.lczq.dbquery.param.RestResponse;
+import com.lczq.dbquery.param.RestResponseBuilder;
 import com.lczq.dbquery.service.SignService;
+import com.lczq.dbquery.util.CacheUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 import java.util.List;
 
@@ -31,6 +34,9 @@ public class DBMgtController
     @Autowired
     private SignService signService;
 
+    @Resource
+    private CacheUtil cacheUtil;
+
     @GetMapping("/queryconfig/{selectId}")
     public QueryConfig getQueryConfig(@PathVariable String selectId)
     {
@@ -43,13 +49,12 @@ public class DBMgtController
         return queryMapper.queryConfigBySelectId(selectId) != null;
     }
 
-    @PostMapping(value="/queryconfig", consumes = "application/json")
+    @PostMapping(value = "/queryconfig", consumes = "application/json")
     public RestResponse<String> saveQueryConfig(@RequestBody QueryConfig queryConfig)
     {
         queryMapper.saveQueryConfig(queryConfig);
         return new RestResponse<>(200, "success");
     }
-
 
     @DeleteMapping("/queryconfig/{selectId}")
     public RestResponse<String> deleteQueryConfig(@PathVariable String selectId)
@@ -65,10 +70,10 @@ public class DBMgtController
         return queryMapper.queryParamsBySelectId(selectId);
     }
 
-    @PostMapping(value="/queryparams", consumes = "application/json")
+    @PostMapping(value = "/queryparams", consumes = "application/json")
     public RestResponse<String> saveQueryParams(@RequestBody List<QueryParams> queryParams)
     {
-        for( QueryParams qp: queryParams ) {
+        for (QueryParams qp : queryParams) {
             queryMapper.saveQueryParams(qp);
         }
         return new RestResponse<>(200, "success");
@@ -84,10 +89,10 @@ public class DBMgtController
     @PostMapping(value = "/gen_sign/{applier}", produces = "application/json;charset=UTF-8")
     public SignEntity genSign(@PathVariable String applier, @RequestHeader MultiValueMap<String, String> headers)
     {
-        if (headers.isEmpty() || ! headers.containsKey("secret")) {
+        if (headers.isEmpty() || !headers.containsKey("secret")) {
             return null;
         }
-        if (! "lczq".equals(headers.getFirst("secret"))) {
+        if (!"lczq".equals(headers.getFirst("secret"))) {
             return null;
         }
         if (null == applier || applier.isEmpty()) {
@@ -96,4 +101,11 @@ public class DBMgtController
         return signService.genSign(applier);
     }
 
+    @DeleteMapping(value = "/delete_redis_keys/{selectId}", produces = "application/json")
+    public RestResponse<String> deleteRedisKeys(@PathVariable String selectId)
+    {
+        int deleteNum;
+        deleteNum = cacheUtil.deleteKeys(selectId);
+        return RestResponseBuilder.succ((long) deleteNum, "");
+    }
 }
