46254061d68f9f4f0513bec32ec2a501b76ee035
赵卫国 <zhaoweiguo@lczq.com>
2023-07-11 16:07:57 +0800

    remove unused imports

--- src/main/java/com/lczq/dbquery/service/DBQueryService.java
+++ src/main/java/com/lczq/dbquery/service/DBQueryService.java
@@ -1,9 +1,7 @@
 package com.lczq.dbquery.service;
 
 import com.lczq.dbquery.entities.QueryResult;
-import javafx.util.Pair;
 
-import java.util.List;
 import java.util.Map;
 
 public interface DBQueryService
