026234f17087b93b573c5b34ff05bfec49a4c735
wgzhao <wgzhao@gmail.com>
2024-01-28 22:41:04 +0800

    Add Test

---
+++ src/test/java/com/github/wgzhao/dbquery/TestAuth.java
@@ -0,0 +1,29 @@
+package com.github.wgzhao.dbquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestAuth {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAuth() throws Exception {
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType("application/json")
+                        .content("{\"username\":\"admin\",\"password\":\"admin123\"}"))
+                .andExpect(status().isOk());
+
+    }
+}
