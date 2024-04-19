f5bcb62de1120cb2ddfe8101146d584ebb18e324
wgzhao <wgzhao@gmail.com>
2024-04-19 09:41:53 +0800

    Add support for post query to resolve query field data type

--- src/main/java/com/github/wgzhao/dbquery/service/impl/DBQueryServiceImpl.java
+++ src/main/java/com/github/wgzhao/dbquery/service/impl/DBQueryServiceImpl.java
@@ -76,7 +76,7 @@ public class DBQueryServiceImpl
             throw new ParamException("查询ID " + selectId + " 未找到数据源");
         }
         //fill back with real execute sql statement
-        logger.info("execute sql is {}", executeSql);
+        logger.info("execute sql is:\n {}", executeSql);
         //async save executed sql to db
         queryLogRepo.save(new QueryLog(0, appId, selectId, executeSql));
         queryConfig.setQuerySql(executeSql);
